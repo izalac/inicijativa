62005fa4d2837f9c4f263deeb6d155230b462af3
izalac <32553994+izalac@users.noreply.github.com>
2017-10-06 12:50:25 +0200

    Update inicijativa-HR.py
    Added features

--- inicijativa-HR.py
+++ inicijativa-HR.py
@@ -1,7 +1,20 @@
+import os, sys
 ime = []
 inicijativa = []
+napomena = []
 runda = 0
-print("Inicijativa 1.0HR (c) 2017 Ivan Žalac\n")
+def promjena(y, inicijativa):
+    while True:
+        try:
+            inic = int(input("Nova inicijativa: "))
+        except ValueError:
+            continue
+        else:
+            if inic < inicijativa[y]:
+                inicijativa[y] = inic
+            break
+
+print("Inicijativa 1.1HR (c) 2017 Ivan Žalac\n")
 while True:
     try:
         n = int(input("Broj sudionika: "))
@@ -9,10 +22,10 @@ while True:
         continue
     else:
         break
-
 for i in range (n):
     print("\nSudionik br. " + str(i+1))
     ime.append(input("Ime: "))
+    napomena.append(" ")
     while True:
         try:
             inic = int(input("Inicijativa: "))
@@ -23,10 +36,28 @@ for i in range (n):
             break
 
 inicijativa, ime = (list(t) for t in zip(*reversed(sorted(zip(inicijativa, ime)))))
-
 while 1:
-    runda+=1;
-    print("\n\nRunda ", runda)
-    for i in range(0,n):
-        print("Na redu je " + ime[i] + " (" + str(inicijativa[i]) + ")")
-        input("Enter za nastavak, ctrl+c za prekid...")
+    runda += 1
+    i = 0
+    print("\n\nRunda " + str(runda) + "\nNaredbe: (C)ekaj, (I)duća runda, (N)apomena, (U)kloni, (R)eset, (K)raj\nili samo pritisnite Enter za iduće po inicijativi.\n")
+    while i < n:
+        if inicijativa[i] == -256:
+            upit="i"
+        else:
+            upit = input("Na redu je " + ime[i] + " (" + str(inicijativa[i]) + ") # " + napomena[i] + " # ")
+        if upit.lower() == 'c':
+            promjena(i, inicijativa)
+            inicijativa, ime, napomena = (list(t) for t in zip(*reversed(sorted(zip(inicijativa, ime, napomena)))))
+        elif upit.lower() == 'i':
+            break
+        elif upit.lower() == 'n':
+            napomena[i] = input ("Napomena: ")
+        elif upit.lower() == 'u':
+            inicijativa[i] = -256
+            inicijativa, ime, napomena = (list(t) for t in zip(*reversed(sorted(zip(inicijativa, ime, napomena)))))
+        elif upit.lower() == 'r':
+            os.execl(sys.executable, sys.executable, *sys.argv)
+        elif upit.lower() == 'k':
+            sys.exit()
+        else:
+            i += 1
